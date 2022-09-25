7eab6eab1a6dabc32dfaa27a8618f54d82c3af6f
levi-viana <levixv3@gmail.com>
2022-09-25 15:11:27 -0300

    oiiu

--- ex022.py
+++ ex022.py
@@ -3,17 +3,11 @@
 – Quantas letras ao todo (sem considerar espaços)[ok].
 – Quantas letras tem o primeiro nome.[ok]'''
 
-nome = str(input('Digite Seu Nome Completo: '))
+nome = str(input('Digite Seu Nome Completo: ')).strip()
 # Fiz tua em uma única variável (n ficou agradavel, mas funcionou)
-maiusculas = (nome.upper()) , (nome.lower())
-
-# Removi espaços de ANTES e depois do nome
-numero = len(nome.strip())
-
-# separei variavel em lista (split)
-lista = nome.split()
-primeiro = len(lista[0]) #criei uma nova variavel  puxando o LEN e a posição da variavel
-
-print(maiusculas)
-print(numero)
-print(primeiro)
+print('O seu nome com letras maiusculas fica {}'.format(nome.upper()))
+print('O seu nome com letras minusculas fica {}'.format(nome.lower()))
+# Fiz tua em uma única variável (n ficou agradavel, mas funcionou)
+print('O seu nome tem ao todo o numero de {} letras'.format(len(nome)-nome.count(' ')))
+separa = nome.split()
+print('Seu primeiro nome é {} e ele tem {} letras'.format(separa[0], len(separa[0])))
