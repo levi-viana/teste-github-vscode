5c96b8b46167a5f6ff764fd14d7883bd192ab7ae
levi-viana <levixv3@gmail.com>
2022-09-26 20:51:06 -0300

    initial commit

--- ex025.py
+++ ex025.py
@@ -1,1 +1,3 @@
 '''Crie um programa que leia o nome de uma pessoa e diga se ela tem “SILVA” no nome.'''
+nome = str(input('Digite seu nome: ')).strip()
+print('Nome seu nome tem Silva ? {}'.format('SILVA' in nome.upper()))
